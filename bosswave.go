65566be8ee50db3c9f7f1833284b77743481ddcd
Gabe Fierro <gtfierro225@gmail.com>
2018-01-05 11:19:01 -0800

    parse duration, return errors

--- bosswave.go
+++ bosswave.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
 	"time"
 
+	//"github.com/gtfierro/pundat/dots"
 	"github.com/immesys/bw2bind"
 	"github.com/pborman/uuid"
 	"github.com/pkg/errors"
 )
 
+var dur_re = regexp.MustCompile(`(\d+)(\w+)`)
+
 type QueryTimeParams struct {
 	T0         string
 	T1         string
-	WindowSize uint64
+	WindowSize string
 	Aligned    bool
 }
 
@@ -20,9 +26,8 @@ type mdalQuery struct {
 	Selectors   []int
 	Variables   []VarParams
 	// serialization-friendly time parameters
-	Time   QueryTimeParams
-	Params Params
-	Nonce  string
+	Time  QueryTimeParams
+	Nonce string
 }
 
 type mdalResponse struct {
@@ -52,6 +57,18 @@ func RunBosswave(c *Core) error {
 		return err
 	}
 
+	publishErr := func(nonce, signaluri string, err error) {
+		po, err := bw2bind.CreateMsgPackPayloadObject(ResponsePID, map[string]interface{}{"Nonce": nonce, "error": err.Error()})
+		if err != nil {
+			log.Error(errors.Wrap(err, "Error marshalling results (msgpack)"))
+			return
+		}
+		if err := iface.PublishSignal(signaluri, po); err != nil {
+			log.Error(errors.Wrapf(err, "Could not publish on %s", iface.SignalURI(signaluri)))
+			return
+		}
+	}
+
 	handleQuery := func(msg *bw2bind.SimpleMessage) error {
 		var inq mdalQuery
 		var query Query
@@ -60,6 +77,9 @@ func RunBosswave(c *Core) error {
 		if po == nil {
 			return nil
 		}
+
+		fromVK := msg.From
+		signaluri := fromVK[:len(fromVK)-1]
 
 		if obj, ok := po.(bw2bind.MsgPackPayloadObject); !ok {
 			err = errors.New("Payload 2.0.10.3 was not MsgPack")
@@ -76,23 +96,37 @@ func RunBosswave(c *Core) error {
 		for _, s := range inq.Selectors {
 			query.Selectors = append(query.Selectors, Selector(s))
 		}
-		query.Params = inq.Params
 		query.Variables = inq.Variables
 		t0, err := time.Parse("2006-01-02 15:04:05", inq.Time.T0)
 		if err != nil {
 			err = errors.Wrapf(err, "Could not parse T0 (%s)", inq.Time.T0)
 			log.Error(err)
+			publishErr(inq.Nonce, signaluri, err)
 			return err
 		}
 		t1, err := time.Parse("2006-01-02 15:04:05", inq.Time.T1)
 		if err != nil {
 			err = errors.Wrapf(err, "Could not parse T1 (%s)", inq.Time.T1)
 			log.Error(err)
+			publishErr(inq.Nonce, signaluri, err)
 			return err
 		}
+		//requestedRange := dots.NewTimeRange(t0, t1)
 		query.Time.T0 = t0
 		query.Time.T1 = t1
-		query.Time.WindowSize = inq.Time.WindowSize
+
+		if inq.Time.WindowSize != "" {
+			dur, err := ParseDuration(inq.Time.WindowSize)
+			if err != nil {
+				err = errors.Wrapf(err, "Could not parse WindowSize (%s)", inq.Time.WindowSize)
+				log.Error(err)
+				publishErr(inq.Nonce, signaluri, err)
+				return err
+			}
+			query.Time.WindowSize = uint64(dur.Nanoseconds())
+		} else {
+			query.Time.WindowSize = 0
+		}
 		query.Time.Aligned = inq.Time.Aligned
 
 		log.Info("Serving query", query)
@@ -101,6 +135,7 @@ func RunBosswave(c *Core) error {
 		if err != nil {
 			err = errors.Wrap(err, "Could not run query")
 			log.Error(err)
+			publishErr(inq.Nonce, signaluri, err)
 			return err
 		}
 
@@ -109,6 +144,7 @@ func RunBosswave(c *Core) error {
 		if err != nil {
 			err = errors.Wrap(err, "Error marshalling results")
 			log.Error(err)
+			publishErr(inq.Nonce, signaluri, err)
 			return err
 		}
 		log.Debug(len(packed))
@@ -121,14 +157,14 @@ func RunBosswave(c *Core) error {
 		if err != nil {
 			err = errors.Wrap(err, "Error marshalling results (msgpack)")
 			log.Error(err)
+			publishErr(inq.Nonce, signaluri, err)
 			return err
 		}
 
-		fromVK := msg.From
-		signaluri := fromVK[:len(fromVK)-1]
 		if err := iface.PublishSignal(signaluri, po); err != nil {
 			err = errors.Wrapf(err, "Could not publish on %s", iface.SignalURI(signaluri))
 			log.Error(err)
+			publishErr(inq.Nonce, signaluri, err)
 			return err
 		}
 
@@ -144,4 +180,38 @@ func RunBosswave(c *Core) error {
 	}
 
 	return nil
+}
+
+func ParseDuration(expr string) (time.Duration, error) {
+	var d time.Duration
+	results := dur_re.FindAllStringSubmatch(expr, -1)
+	if len(results) == 0 {
+		return d, errors.New("Invalid. Must be Number followed by h,m,s,us,ms,ns,d")
+	}
+	num := results[0][1]
+	units := results[0][2]
+	i, err := strconv.ParseInt(num, 10, 64)
+	if err != nil {
+		return d, err
+	}
+	d = time.Duration(i)
+	switch units {
+	case "h", "hr", "hour", "hours":
+		d *= time.Hour
+	case "m", "min", "minute", "minutes":
+		d *= time.Minute
+	case "s", "sec", "second", "seconds":
+		d *= time.Second
+	case "us", "usec", "microsecond", "microseconds":
+		d *= time.Microsecond
+	case "ms", "msec", "millisecond", "milliseconds":
+		d *= time.Millisecond
+	case "ns", "nsec", "nanosecond", "nanoseconds":
+		d *= time.Nanosecond
+	case "d", "day", "days":
+		d *= 24 * time.Hour
+	default:
+		err = fmt.Errorf("Invalid unit %v. Must be h,m,s,us,ms,ns,d", units)
+	}
+	return d, err
 }
